714e1945def64afda5af67e3af07e959a414595c
Brave0211 <abduqodirovjasurbek11@gmail.com>
2022-08-25 17:19:20 +0500

    react-book

--- src/components/Searchhome/Searchhome.js
+++ src/components/Searchhome/Searchhome.js
@@ -1,25 +1,26 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import "./searchhome.css";
-import axios from "axios"
 
 export const Searchhome = () => {
+  const [search, setSearch] = useState("");
 
-  const[search,setSearch] = useState("")
-
-  useEffect(()=>{
+  useEffect(() => {
     axios
-      .get("https://book-service-layer.herokuapp.com/author/search")
-      .then(res=>setSearch(res.data))
-      .catch(err=>console.log(err))
-  })
-
+      .get(`https:/book-service-layer.herokuapp.com/author/search/`)
+      .then((res) => setSearch(res.data))
+      .catch((err) => console.log(err));
+  });
 
   return (
     <div className="search">
       <div className="search__container">
         <p>QIDIRISH</p>
         <form className="search__form">
-          <input type="text" placeholder="Adiblar, kitoblar, audiolar, maqolalar..." />
+          <input
+            type="text"
+            placeholder="Adiblar, kitoblar, audiolar, maqolalar..."
+          />
           <button type="submit">Izlash</button>
         </form>
       </div>
